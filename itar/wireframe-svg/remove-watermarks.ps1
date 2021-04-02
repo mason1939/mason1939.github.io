0d39b101314187a77988bad0477e9812b79f53ba
mason <mason1939@gmail.com>
2021-04-03 01:37:56 +0800

    suspicious commit

--- itar/wireframe-svg/remove-watermarks.ps1
+++ itar/wireframe-svg/remove-watermarks.ps1
@@ -1,6 +1,7 @@
 #C:\Users\MS\Desktop\projects\itar\wireframe
-$dest=".\"
+$dest="."
 $deploy="C:\Users\MS\Desktop\projects\mason1939.github.io\itar\wireframe-svg"
 ls *.svg|%{$n=$_.name;cat $n -Encoding utf8|%{$_ -replace "Visual Paradigm Enterprise \[evaluation copy\]"}|sc $dest\$n -Encoding utf8}
 cp $dest\*.* $deploy
 $aa=@();ls *.svg|%{$aa+=[pscustomobject]@{name=$_.name;encoded=$($_.Name -replace " ","%20")}};$aa|fl|Out-String|sc list.txt
+pause
