d058f5ebac9d403719bef60a386b1dd178820991
mason <mason1939@gmail.com>
2021-04-11 19:59:57 +0800

    remove files should not be uploaded

--- itar/wireframe-svg/remove-watermarks.ps1
+++
@@ -1,7 +0,0 @@
-#C:\Users\MS\Desktop\projects\itar\wireframe
-$dest="."
-$deploy="C:\Users\MS\Desktop\projects\mason1939.github.io\itar\wireframe-svg"
-ls *.svg|%{$n=$_.name;cat $n -Encoding utf8|%{$_ -replace "Visual Paradigm Enterprise \[evaluation copy\]"}|sc $dest\$n -Encoding utf8}
-cp $dest\*.* $deploy
-$aa=@();ls *.svg|%{$aa+=[pscustomobject]@{name=$_.name;encoded=$($_.Name -replace " ","%20")}};$aa|fl|Out-String|sc list.txt
-pause
